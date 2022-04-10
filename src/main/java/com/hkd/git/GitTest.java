5b680e3c4efec2e514278a72b2b91c33fe877e2e
xiaosachen <chenxiaosa123@163.com>
2022-04-10 22:48:39 +0800

    push commit

--- src/main/java/com/hkd/git/GitTest.java
+++ src/main/java/com/hkd/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("hello git!4");
         System.out.println("master test");
         System.out.println("hot-fix git");
+        System.out.println("push git");
     }
 
 }
