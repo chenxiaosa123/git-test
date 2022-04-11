81a6cdfd16fe315044fbdcc6bd28d00bbb26e159
chenxiaosa123 <99639691+chenxiaosa123@users.noreply.github.com>
2022-04-11 10:35:41 +0800

    poll commit

--- src/main/java/com/hkd/git/GitTest.java
+++ src/main/java/com/hkd/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hot-fix git");
         System.out.println("push git");
+        System.out.println("pull git");
     }
 
 }
